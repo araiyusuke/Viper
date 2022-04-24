25b77ed05d2c7f99c9f3711c44c11c91bb437050
araiyusuke <arai-t-ryousan06@tanita.co.jp>
2022-04-24 10:58:09 +0900

    Initial Commit

---
+++ Viper/ViperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViperApp.swift
+//  Viper
+//
+//  Created by 名前なし on 2022/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ViperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
